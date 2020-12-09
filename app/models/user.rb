c5a383856b69fbdc38364ac526221718276a0f9d
Takamiti-58 <73811747+Takamiti-58@users.noreply.github.com>
2020-12-09 16:06:44 +0900

    ユーザー管理機能３

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-with_options presence: true do
-    validates :nickname,
-    validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-    validates :encrypted_password, length: { minimum: 7 }
-    validates :first_name, format: { with: /\A[一-龥ぁ-ん]/ }
-    validates :last_name, format: { with: /\A[一-龥ぁ-ん]/ }
-    validates :first_name_kana, format: { with: /\A[ぁ-ん]/ }
-    validates :last_kana, format: { with: /\A[ぁ-ん]/ }
-    validates :birth_day
-  end
+  validates :nickname,           presence: true
+  validates :email,              presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password,           presence: true, length: { minimum: 7 }
+  validates :encrypted_password, presence: true, length: { minimum: 7 }
+  validates :first_name,         presence: true, format: { with: /\A[一-龥ぁ-ん]/ }
+  validates :last_name,          presence: true, format: { with: /\A[一-龥ぁ-ん]/ }
+  validates :first_name_kana,         presence: true, format: { with: /\A[ぁ-ん]/ }
+  validates :last_name_kana,          presence: true, format: { with: /\A[ぁ-ん]/ }
+  validates :birth_day,         presence: true
 end
