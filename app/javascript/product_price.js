52a3d9629540cda374bdfa09547676b715af78f2
Takamiti-58 <73811747+Takamiti-58@users.noreply.github.com>
2020-12-14 15:07:48 +0900

    商品出品機能修正２

--- app/javascript/product_price.js
+++ app/javascript/product_price.js
@@ -9,12 +9,3 @@ window.addEventListener('load', function() {
     addTaxProfit.innerHTML = Math.floor(inputValue * 0.9 )
   })
 });
-
-// ①priceInputで、入力がされたら、以下の処理を行う　※記事参照
-// ②priceInputに入力された「値」を取得する　※記事参照
-
-// ③手数料・販売履歴を貼り付ける場所を取得する（getElementById）
-
-// ④②で取得した値を計算する（手数料１０％、利益＝値ー手数料）
-
-// ⑤③で取得した住所に④で計算した値を貼り付ける
--- app/models/product.rb
+++ app/models/product.rb
@@ -9,13 +9,15 @@ class Product < ApplicationRecord
   belongs_to_active_hash :shipping_cost
   belongs_to_active_hash :prepare
 
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :status_id
+    validates :shipping_cost_id
+    validates :shipping_days_id
+    validates :prefecture_id
+    validates :category_id
+  end
   validates :name, presence: true, length: { maximum: 40 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :description, presence: true, length: { maximum: 1000 }
-  validates :status_id, presence: true, numericality: { other_than: 1 }
-  validates :shipping_cost_id, presence: true, numericality: { other_than: 1 }
-  validates :shipping_days_id, presence: true, numericality: { other_than: 1 }
-  validates :prefecture_id, presence: true, numericality: { other_than: 1 }
-  validates :category_id, presence: true, numericality: { other_than: 1 }
   validates :image, presence: true
 end
