8bb0375f1cbe98415bda5a8172f326ccf2efc252
Takamiti-58 <73811747+Takamiti-58@users.noreply.github.com>
2020-12-16 19:46:46 +0900

    商品削除機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,6 +20,12 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @products = Product.find(params[:id])
+    @products.destroy
+    redirect_to root_path
+  end
+
   def edit
     if @products.user.id != current_user.id
       redirect_to root_path
